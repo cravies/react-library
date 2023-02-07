f054b9de9641bf4867e098f612d9665eba97ab64
ben <ben.conor.reese.cravens@gmail.com>
2023-02-08 10:33:27 +1300

    skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
+import BookShelf from './BookShelf'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </div>
+    <>
+    <h1> Ben's Library </h1>
+    <BookShelf />
+    <input type="text" />
+    <button>Add book to shelf</button>
+    <button>Clear shelf</button>
+    <div>0 books read.</div>
+    </>
   )
 }
 
---
+++ src/BookShelf.jsx
@@ -0,0 +1,11 @@
+import './BookShelf.css'
+
+function BookShelf() {
+  return (
+    <div className="shelf">
+      <p> A shelf element. </p>
+    </div>
+  )
+}
+
+export default BookShelf
